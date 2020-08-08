f0b412b9fa268a7d606bddd90dc2cfcfab08c7f8
Oluwaseun Peter <adepeter26@gmail.com>
2020-08-08 20:06:53 +0100

    Update settings.py

--- iiots_project/settings.py
+++ iiots_project/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = PurePath(BASE_DIR).joinpath('static')
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
